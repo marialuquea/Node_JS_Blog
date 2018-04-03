7f921c2b49270488c52e2e4c40e587cfb48017db
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-04-03 23:36:02 +0100

    can now login and messages cssed

--- models/user.js
+++ models/user.js
@@ -28,6 +28,24 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function(id, callback){
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+  //create one object to find
+  var query = {username: username};
+  // findOne will only return one object
+  User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    // res == true
+    callback(null, isMatch);
+  });
+}
+
 module.exports.createUser = function(newUser, callback){
   //encrypt password using bcrypt
   bcrypt.genSalt(10, function(err, salt){
