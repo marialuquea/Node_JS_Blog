b5ce559363c8f2aee2e39ff1e57e1b8daab47c8c
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-04-07 15:20:16 +0100

    edit and delete posts works!!!

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,7 @@ router.get('/show/:id', function(req, res, next) {
 	});
 });
 
+//--------------ADD POST--------------------
 router.get('/add', function(req, res, next) {
 	var categories = db.get('categories');
 
@@ -25,6 +26,7 @@ router.get('/add', function(req, res, next) {
   		});
 	});
 });
+
 
 router.post('/add', upload.single('mainimage'), function(req, res, next) {
   // Get Form Values
@@ -73,7 +75,106 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
 	}
 });
 
+//---------------------EDIT POST---------------------
+router.get('/editpost/:id', function(req, res, next){
+	var posts = db.get('posts');
+	var categories = db.get('categories');
 
+	categories.find({}, {}, function(err, categories){
+
+		posts.find({'_id': db.id(req.params.id)}, {}, function(err, posts){
+
+			req.app.locals.mainimage = posts[0].mainimage;
+			req.app.locals.postid = req.params.id;
+			res.render('edit', {
+				posts: posts,
+				categories: categories
+			});
+		});
+	});
+});
+
+router.post('/editpost', upload.single('mainimage'), function(req, res, next){
+
+	var title = req.body.title;
+	var body = req.body.body;
+	var category = req.body.category;
+  var postid = req.app.locals.postid;
+
+	// Check Image Upload
+	if(req.file){
+		var mainimage = req.file.filename
+	} else {
+		var mainimage = 'noimage.jpg';
+	}
+
+  	// Form Validation
+	req.checkBody('category','Name field is required').notEmpty();
+	req.checkBody('body', 'Body field is required').notEmpty();
+	req.checkBody('title', 'Title field is required').notEmpty();
+
+  // Check erros
+	var errors = req.validationErrors();
+
+	if(errors){
+
+		var posts = db.get('posts');
+		var categories = db.get('categories');
+		categories.findById({}, {}, function(err, categories){
+			posts.findById({'_id': db.id(req.app.locals.postid)}, {}, function(err, posts){
+
+				res.render('editpost', {
+
+					'posts' : posts,
+					'categories' : categories
+				});
+			});
+		});
+	} else{
+
+		var mainimage;
+		var posts = db.get('posts');
+
+		if(req.file){
+			mainimage = req.file.filename;
+		} else{
+			mainimage = req.app.locals.mainimage;
+		}
+
+		posts.update({
+			'_id' : postid
+		}, {
+			$set: {
+				'title': title,
+				'body': body,
+				'category': category,
+				'mainimage': mainimage
+			}
+		}, function(err, doc){
+
+			req.app.locals.postid = '';
+			req.app.locals.mainimage = '';
+			req.flash('success', 'Post Edited');
+			res.location('/posts/show/'+postid);
+			res.redirect('/posts/show/'+postid);
+		});
+	}
+});
+
+
+//------------------DELETE POST----------------------
+router.get('/deletepost/:id', function(req, res, next){
+	var  posts = db.get('posts');
+	//remove post from post collection
+	posts.remove({_id: db.id(req.params.id)});
+	// Redirect to blog
+	res.location('/');
+	res.redirect('/');
+});
+
+
+
+//---------------------ADD COMMENT-------------------
 router.post('/addcomment', function(req, res, next) {
   // Get Form Values
   var name = req.body.name;
@@ -126,5 +227,7 @@ router.post('/addcomment', function(req, res, next) {
 		});
 	}
 });
+
+
 
 module.exports = router;
